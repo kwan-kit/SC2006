76bd123c7a3fbea528013b548f184adf343a88df
ranulaaa <157560067+ranulaaa@users.noreply.github.com>
2024-10-25 17:54:29 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,27 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.use((req, res, next) => {
   req.body.targetDistance = 5; // 5 km arbitrary target distance
   next();
 });
 
-app.use('/api/parks', parks);
-app.use('/api/calculate-distances', calculatedistanceRouter);
+app.get('/', (req, res) => {
+  res.json({ message: 'Training Plan API is running' });
+});
+
+//app.use('/api/parks', parks);
+//app.use('/api/calculate-distances', calculatedistanceRouter);
 // app.use('/api/geolocation', geolocationRouter);
 // app.use('/api/closest-point', closestPointRouter);
-app.use('/plan', createTrainingPlan);
+app.use('/training', createTrainingPlan);
 app.use('/save', saveTrainingPlan)
 
 app.listen(process.env.PORT, () => {
