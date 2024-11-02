0d84414c4467d8eac02b759219f92a2f8d30ecbc
ranulaaa <157560067+ranulaaa@users.noreply.github.com>
2024-11-03 05:13:24 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const sessionMiddleware = require('./utilities/sessionConfig'); 
 const bodyParser = require('body-parser');
-const saveTrainingPlan = require('./routes/saveTrainingPlan');
 const createTrainingPlan = require('./routes/createTrainingPlan');
 const gymList = require('./routes/gymList');
 const parkList = require('./routes/parkList');
 const strava = require('./routes/stravaConnect');
-const userRoutes = require('./routes/userRoutes');
-const userCredentialsRouter = require('./routes/userCredentials');
+const runReportRouter = require('./routes/saveRunReports');
+const gymReportRouter = require('./routes/saveGymReport');
 
 const mongoose = require('mongoose');
 const { check, validationResult } = require('express-validator');
@@ -30,12 +28,12 @@ mongoose.connect(process.env.MONGODB_URI, {
 // });
 
 
-// app.use('/training', createTrainingPlan);
-// app.use('/save', saveTrainingPlan)
+app.use('/training', createTrainingPlan);
+app.use('/save', runReportRouter);
+app.use('/record', gymReportRouter);
 // app.use('/gym',gymList);
 // app.use('/park',parkList);
 // app.use('/strava',strava);
-app.use('/user', userCredentialsRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
