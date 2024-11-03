8059d361b903cda7df660e1dbf8cfe0bdfa809af
sultanyichen <tanyichen04@gmail.com>
2024-11-03 12:29:42 +0800

    added session storage

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const sessionMiddleware = require('./utilities/sessionConfig'); 
 const bodyParser = require('body-parser');
